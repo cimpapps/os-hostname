a554f64d0659201129efb382cd644e853e8db31b
Catalin.Cimpoeru <catalin.cimpoeru@orange.com>
2020-02-23 12:49:43 +0200

    session afinity

--- src/main/kotlin/com/example/kubernetes/oshostname/config/Config.kt
+++ src/main/kotlin/com/example/kubernetes/oshostname/config/Config.kt
@@ -1,21 +1,12 @@
 package com.example.kubernetes.oshostname.config
 
+import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.MediaType
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.client.RestTemplate
-import java.util.*
-
 
 @Configuration
 class Config {
     @Bean
-    fun restTemplate(): RestTemplate {
-        val restTemplate = RestTemplate()
-        val converter = MappingJackson2HttpMessageConverter()
-        converter.supportedMediaTypes = Collections.singletonList(MediaType.TEXT_HTML)
-        restTemplate.messageConverters.add(converter)
-        return restTemplate
-    }
+    fun restTemplate(): RestTemplate = RestTemplateBuilder().build()
 }
